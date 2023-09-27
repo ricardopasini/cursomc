706baecc547a2a408d9dc391c5e5b31062c4cb98
Ricardo Pasini <ricardo.pasini@gmail.com>
2023-09-27 17:35:53 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/ricardopasini/cursomc/CursomcApplication.java
+++ src/main/java/com/ricardopasini/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.ricardopasini.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.ricardopasini.cursomc.domain.Cidade;
 import com.ricardopasini.cursomc.domain.Cliente;
 import com.ricardopasini.cursomc.domain.Endereco;
 import com.ricardopasini.cursomc.domain.Estado;
+import com.ricardopasini.cursomc.domain.Pagamento;
+import com.ricardopasini.cursomc.domain.PagamentoComBoleto;
+import com.ricardopasini.cursomc.domain.PagamentoComCartao;
+import com.ricardopasini.cursomc.domain.Pedido;
 import com.ricardopasini.cursomc.domain.Produto;
+import com.ricardopasini.cursomc.domain.enums.EstadoPagamento;
 import com.ricardopasini.cursomc.domain.enums.TipoCliente;
 import com.ricardopasini.cursomc.repositories.CategoriaRepository;
 import com.ricardopasini.cursomc.repositories.CidadeRepository;
 import com.ricardopasini.cursomc.repositories.ClienteRepository;
 import com.ricardopasini.cursomc.repositories.EnderecoRepository;
 import com.ricardopasini.cursomc.repositories.EstadoRepository;
+import com.ricardopasini.cursomc.repositories.PagamentoRepository;
+import com.ricardopasini.cursomc.repositories.PedidoRepository;
 import com.ricardopasini.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -41,6 +49,12 @@ public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -90,6 +104,22 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 		
 	}
 
--- src/main/java/com/ricardopasini/cursomc/domain/Cliente.java
+++ src/main/java/com/ricardopasini/cursomc/domain/Cliente.java
@@ -38,9 +38,8 @@ public class Cliente implements Serializable {
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
-	public Cliente() {
-		
-	}
+	@OneToMany(mappedBy = "cliente")
+	List<Pedido> pedidos = new ArrayList<>();
 
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
 		super();
@@ -105,6 +104,18 @@ public class Cliente implements Serializable {
 
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
+	}
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+
+	public Cliente() {
+		
 	}
 
 	@Override
